8af374ec6a923468df9a00e01991a7e1c2ed3724
KosukeYamaguchi <kosuke.yamaguchi@arsaga.jp>
2023-09-05 20:20:42 +0900

    MUIのSelectとRHFのControllerとの連携

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,73 @@
-import { Stack, TextField, Button } from '@mui/material'
+import { Stack, FormControl, InputLabel, Select, MenuItem, FormHelperText, Button } from '@mui/material'
 import { useForm, SubmitHandler, Controller } from 'react-hook-form'
 
 // 1. 入力値の定義を作成します。
  type Inputs = {
-  name: string
+   area: number | ''
  }
 
+// 2. useFormで必要な関数を取得し、デフォルト値を指定します。
 function App() {
-  // 2. useFormで必要な関数を取得し、デフォルト値を指定します。
-  const {
-    control,
-    handleSubmit,
-  } = useForm<Inputs>({
-    defaultValues: { name: '初期値です' }
-  })
+   const {
+    //Controllerを使うには①、useFormからcontrolを取得
+     control,
+     handleSubmit,
+   } = useForm<Inputs>({
+     defaultValues: { area: 6 }
+   })
 
-  // 3. 検証ルールを指定します。
-  const validationRules = {
-    name: {
-      required: '名前を入力してください。',
-      minLength: { value: 4, message: '4文字以上で入力してください。' }
-    }
-  }
-  
-  // 4. サブミット時の処理を作成します。
-  // 検証が成功すると呼び出され、引数で入力値が渡ってきます。
-  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
-    console.log(`submit: ${data.name}`)
-  }
+// 3. 検証ルールを指定します。
+   const validationRules = {
+     area: {
+       validate: (value:number | '') => value !== '' || 'いずれかを選択してください。'
+     }
+   }
 
-  return (
-  // {/* 5. form要素のonSubmitに1.で取得しているhandleSubmitを指定します */}
-    <Stack component="form" noValidate 
-    onSubmit={handleSubmit(onSubmit)} 
-    spacing={2} sx={{ m: 2, width: '25ch' }}>
+ // 4. サブミット時の処理を作成します。
+   const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
+     console.log(`submit: ${data.area}`)
+   }
 
-       {/* 6.Controllerコンポーネントで TextFieldをReactHookFormと紐づけます。 */}
+ return (
+//  {/* 5. form要素のonSubmitに1.で取得しているhandleSubmitを指定します */}
+   <Stack component="form" noValidate 
+      onSubmit={handleSubmit(onSubmit)} 
+     spacing={2} 
+     sx={{ m: 2, width: '25ch' }}>
+
+      {/* 6.Controllerコンポーネントで TextFieldをReactHookFormと紐づけます。*/}
+      {/* Controllerを使うには②、ControllerコンポーネントでMUIを囲み、必要なpropsを設定する */}
       <Controller
-        // registerと同様の意味（<input {  ...register("email")} />  と書いていた  )
-        name="name"
-
-        //useFormで取得したcontrolを渡す。決まりごととして覚えておく
+        name="area"
         control={control}
-
-        //バリデーションプロパティが入ったオブジェクトを渡す。
-        rules={validationRules.name}
-
-        //renderには関数を指定する。
-        //引数のfieldにはcontrolで指定したpropsの値が入っている
-        //引数の
-
+        rules={validationRules.area}
         render={({ field, fieldState }) => (
-          <TextField
-            {...field}
-            type="text"
-            label="名前"
-            error={fieldState.invalid}
-            helperText={fieldState.error?.message}
-          />
-        )}
-      />
-      <Button variant="contained" type="submit" >
-        送信する
-      </Button>
-    </Stack>
-  )
+          <FormControl fullWidth error={fieldState.invalid}>
+           <InputLabel id="area-label">地域</InputLabel>
+           <Select
+             labelId="area-label"
+             label="地域" // フォーカスを外した時のラベルの部分これを指定しないとラベルとコントロール線が被る
+              {...field}
+           >
+             <MenuItem value='' sx={{color:'gray'}}>未選択</MenuItem>
+             <MenuItem value={1}>北海道</MenuItem>
+             <MenuItem value={2}>東北</MenuItem>
+             <MenuItem value={3}>関東</MenuItem>
+             <MenuItem value={4}>中部</MenuItem>
+             <MenuItem value={5}>近畿</MenuItem>
+             <MenuItem value={6}>中国</MenuItem>
+             <MenuItem value={7}>四国</MenuItem>
+             <MenuItem value={8}>九州沖縄</MenuItem>
+           </Select>
+            <FormHelperText>{fieldState.error?.message}</FormHelperText>
+         </FormControl>
+       )}
+     />
+     <Button variant="contained" type="submit" >
+       送信する
+     </Button>
+   </Stack>
+ )
 }
 
 export default App;
